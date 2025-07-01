c9a207e3f49eadba97e1acb4e4f7a336de0c6176
imran uddin <iuddin39@gmail.com>
2025-07-01 18:54:43 +0530

    create a new python file arithmetic-operation.py operation

---
+++ number operation/arithmetic-operation.py
@@ -0,0 +1,16 @@
+#this is a simple program on arithmetic operation
+#where we are taking two number from user and perform different operation
+
+num1=int(input('enter first number'))
+num2=int(input("enter second number"))
+sum=num1+num2
+print("sum=",sum)
+subs=num1-num2
+print("substration=",subs)
+divs=num1/num2
+print("decimal division",divs)
+#this is floor division it gives only interger number not floating point number
+div=num1//num2
+print("division",div)
+mul=num1*num2
+print("multiplication=",mul)
