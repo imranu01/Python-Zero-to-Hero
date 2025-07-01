868f523aa5d1cc89195b29f682856201ec708b56
imran uddin <iuddin39@gmail.com>
2025-07-01 19:35:17 +0530

    create new file greated of two

---
+++ number operation/greatest-of-two.py
@@ -0,0 +1,10 @@
+"""this is a program to demostrat if else 
+to find a largest number from two number"""
+
+num1=int(input('enter first number'))
+num2=int(input("enter second number"))
+
+if num1>num2:
+    print("number {0}  is greater".format(num1))
+else:
+    print("number {0}  is greater".format(num2))
